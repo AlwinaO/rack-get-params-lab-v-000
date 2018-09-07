aba8bacfe294bcb64da3a055ce9a1fad6663da78
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-09-07 23:34:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,9 +11,11 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
@@ -23,6 +25,10 @@ class Application
           resp.write "#{c}\n"
         end
       end
+
+    elsif req.path.match(/add/)
+      binding.pry
+
     else
       resp.write "Path Not Found"
     end
